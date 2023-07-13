82723c268d8c22e0cc695595e911aff2ba248083
Carlos de Tena Bellmont <carlos.bellmont@gmail.com>
2023-07-13 16:07:47 +0200

    Proyecto final dragon ball
    Listener

--- DragonBallFinal/app/src/main/java/com/keepcoding/dragonballfinal/MainActivity.kt
+++ DragonBallFinal/app/src/main/java/com/keepcoding/dragonballfinal/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.keepcoding.dragonballfinal
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.core.widget.doAfterTextChanged
 import com.keepcoding.dragonballfinal.databinding.ActivityMainBinding
@@ -9,6 +11,8 @@ import com.keepcoding.dragonballfinal.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private val userTag = "USER_TAG"
+    private val passTag = "PASS_TAG"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         with(binding) {
+            cargarLoginDePreferencias()
             etUser.doAfterTextChanged { editable ->
                 editable?.let {
                     btnLogin.isEnabled =
@@ -29,13 +34,50 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             btnLogin.setOnClickListener {
-                if (etUser.text.contains("@") && etUser.text.contains(".") && etPass.text.length >= 4)
+                if (etUser.text.contains("@") && etUser.text.contains(".") && etPass.text.length >= 4) {
                     Toast.makeText(this@MainActivity, "Login Correcto", Toast.LENGTH_LONG).show()
-                else
+                    if (switchRememberUser.isChecked) guardarLoginEnPreferencias(etUser.text.toString(), etPass.text.toString())
+                } else
                     Toast.makeText(this@MainActivity, "Login Fallido", Toast.LENGTH_LONG).show()
             }
-
+            etUser.setOnFocusChangeListener { view, hasFocus ->
+                if (hasFocus)
+                    Log.w("MainActivity", "etUser tiene el foco")
+                else
+                    Log.w("MainActivity", "etUser ha perdido el foco")
+            }
+            etPass.setOnFocusChangeListener { view, hasFocus ->
+                if (hasFocus)
+                    Log.w("MainActivity", "etPass tiene el foco")
+                else
+                    Log.w("MainActivity", "etPass ha perdido el foco")
+            }
+            switchRememberUser.setOnCheckedChangeListener { _, isChecked ->
+                if (!isChecked) borrarPreferencias()
+            }
         }
         Toast.makeText(this, "Login Fallido", Toast.LENGTH_LONG).show()
+    }
+
+    private fun borrarPreferencias() {
+        getPreferences(Context.MODE_PRIVATE).edit().clear().apply()
+    }
+
+    private fun guardarLoginEnPreferencias(user: String, pass: String) {
+        getPreferences(Context.MODE_PRIVATE).edit()
+            .putString(userTag, user)
+            .putString(passTag, pass)
+            .apply()
+    }
+
+    private fun cargarLoginDePreferencias() {
+        with(binding) {
+            getPreferences(Context.MODE_PRIVATE).apply {
+                etUser.setText(getString(userTag, ""))
+                etPass.setText(getString(passTag, ""))
+                if (etPass.text.isNotEmpty() && etUser.text.isNotEmpty())
+                    switchRememberUser.isChecked = true
+            }
+        }
     }
 }
