8cb7efbabe949af6546b483706a9a2ed26f4ab85
Carlos de Tena Bellmont <carlos.bellmont@gmail.com>
2023-07-13 15:12:28 +0200

    Proyecto final dragon ball
    Listener

--- DragonBallFinal/app/src/main/java/com/keepcoding/dragonballfinal/MainActivity.kt
+++ DragonBallFinal/app/src/main/java/com/keepcoding/dragonballfinal/MainActivity.kt
@@ -2,30 +2,40 @@ package com.keepcoding.dragonballfinal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.widget.doAfterTextChanged
 import com.keepcoding.dragonballfinal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-     private lateinit var binding : ActivityMainBinding
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding =  ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.etUser.doAfterTextChanged { editable ->
-            editable?.let {
-                binding.btnLogin.isEnabled = it.toString().isNotEmpty() && binding.etPass.text.isNotEmpty()
+        with(binding) {
+            etUser.doAfterTextChanged { editable ->
+                editable?.let {
+                    btnLogin.isEnabled =
+                        it.toString().isNotEmpty() && etPass.text.isNotEmpty()
+                }
             }
-        }
-        binding.etPass.doAfterTextChanged { editable ->
-            editable?.let {
-                binding.btnLogin.isEnabled = it.toString().isNotEmpty() && binding.etUser.text.isNotEmpty()
+            etPass.doAfterTextChanged { editable ->
+                editable?.let {
+                    btnLogin.isEnabled =
+                        it.toString().isNotEmpty() && etUser.text.isNotEmpty()
+                }
             }
+            btnLogin.setOnClickListener {
+                if (etUser.text.contains("@") && etUser.text.contains(".") && etPass.text.length >= 4)
+                    Toast.makeText(this@MainActivity, "Login Correcto", Toast.LENGTH_LONG).show()
+                else
+                    Toast.makeText(this@MainActivity, "Login Fallido", Toast.LENGTH_LONG).show()
+            }
+
         }
-        binding.btnLogin.setOnClickListener {
-            // TODO muestra toast de "ok" si, el usuario tiene una @ y un "." y la contraseña tiene más de 4 chars
-        }
+        Toast.makeText(this, "Login Fallido", Toast.LENGTH_LONG).show()
     }
 }
