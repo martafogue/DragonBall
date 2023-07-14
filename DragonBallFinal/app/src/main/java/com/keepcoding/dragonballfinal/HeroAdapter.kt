2cd67f07dc6d956c7b2a9dd56f148948d8024a00
Carlos de Tena Bellmont <carlos.bellmont@gmail.com>
2023-07-14 10:51:17 +0200

    Adapter Largo

--- DragonBallFinal/app/src/main/java/com/keepcoding/dragonballfinal/HeroAdapter.kt
+++ DragonBallFinal/app/src/main/java/com/keepcoding/dragonballfinal/HeroAdapter.kt
@@ -2,18 +2,25 @@ package com.keepcoding.dragonballfinal
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.keepcoding.dragonballfinal.databinding.ItemHeroBinding
 
 class HeroAdapter: RecyclerView.Adapter<HeroAdapter.HeroViewHolder>() {
 
     class HeroViewHolder(val binding: ItemHeroBinding): RecyclerView.ViewHolder(binding.root) {
-        fun bind(nombre: String) {
+        fun bind(nombre: String, position: Int) {
             binding.tvName.text = nombre
+            val bgColorId = if (position % 2 == 0) R.color.dark_orange else R.color.orange
+            binding.root.setBackgroundColor(ContextCompat.getColor(binding.root.context, bgColorId))
+
         }
     }
 
-    var list = listOf("Goku", "Vegeta")
+    var list = List(100) {
+        "Goku $it"
+    }
 
     override fun getItemCount(): Int {
         return list.size
@@ -29,6 +36,6 @@ class HeroAdapter: RecyclerView.Adapter<HeroAdapter.HeroViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: HeroViewHolder, position: Int) {
-        holder.bind(list[position])
+        holder.bind(list[position], position)
     }
 }
